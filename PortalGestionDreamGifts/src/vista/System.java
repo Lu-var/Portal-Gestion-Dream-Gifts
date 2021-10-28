2258a003c6c2c73bde3165d2d0b81a251040b52f
xCrowstone <92612803+xCrowstone@users.noreply.github.com>
2021-10-28 04:30:35 -0300

    Revert "login casi completo con validacion"
    This reverts commit de78d572332faea6311618ed702c03c82b11fd7d.

--- PortalGestionDreamGifts/src/vista/System.java
+++ PortalGestionDreamGifts/src/vista/System.java
@@ -8,15 +8,13 @@ package vista;
  *
  * @author PC
  */
-public class Principal extends javax.swing.JFrame {
+public class System extends javax.swing.JFrame {
 
     /**
      * Creates new form System
      */
-    public Principal() {
+    public System() {
         initComponents();
-        
-         this.setLocationRelativeTo(null);
     }
 
     /**
@@ -127,21 +125,20 @@ public class Principal extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(System.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(System.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(System.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(System.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Principal().setVisible(true);
+                new System().setVisible(true);
             }
         });
     }
