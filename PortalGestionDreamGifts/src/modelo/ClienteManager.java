5b6dd297f29e968395ba26d3f887e5419172f2ac
Luxarx <lucas.vargas@sansano.usm.cl>
2021-10-29 21:09:17 -0300

    Clase ClienteManager creada.

---
+++ PortalGestionDreamGifts/src/modelo/ClienteManager.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+import java.sql.*;
+        
+/**
+ *
+ * @author luvar
+ */
+public class ClienteManager {
+    
+    ConexionRequest intentoConexion = new ConexionRequest();
+    Connection conexion = intentoConexion.conectar();
+    
+    public void AgregarCliente(String RUT, String Nombre, String FechaNacimiento){
+        
+    }
+    
+    
+    
+}
