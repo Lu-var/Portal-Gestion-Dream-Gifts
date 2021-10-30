cb9867a87b146c09444ff95c04c78de2efd97e2d
xCrowstone <92612803+xCrowstone@users.noreply.github.com>
2021-10-30 03:36:22 -0300

    Revert "avance conexion y login"
    This reverts commit 0041e949234a1f65b7b80f93a404afcbf0811174.

--- PortalGestionDreamGifts/src/controlador/LoginController.java
+++ PortalGestionDreamGifts/src/controlador/LoginController.java
@@ -1,8 +1,9 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package controlador;
-import vista.Login;
-import modelo.Conexion;
-import java.sql.ResultSet;
+import vista.*;
 /**
  *
  * @author PC
@@ -11,44 +12,4 @@ public class LoginController {
     public static Login login = new Login();
     public static void mostrar(){ login.setVisible(true); }
     public static void ocultar(){ login.setVisible(false); }
-    
-    //public funcionDelControlador(usuario, contra){ 
-        //Aca realizai la consulta a la base de datos y aplicando otras validaicones si fuese necesario
-        //comparacion
-        // select * from  dreamgifts_
-    //}
-    public void ingreso (String usuario, String password){
-
-       
-        
-        try{
-            
-                   Conexion con = new Conexion();
-       con.conexion();   
-       
-       String query = "SELECT `idUsuario`, `Nombre`, `Password` FROM `usuarios` WHERE  `Nombre` = '" + usuario + "' AND `Password` = '" + password + "'";
-        //System.out.println(query);
-        
-            ResultSet rs = con.select(query);
-        
-            if(rs.next() ){
-                //String idBd = rs.getString("idUsuario");
-                //String nombreBd = rs.getString("Nombre");
-                //String passwordBd = rs.getString("Password");
-                
-                //System.out.println("desde idBd" + idBd);
-                //System.out.println("desde nombreBd" + nombreBd);
-                //System.out.println("desde passwordBd" + passwordBd);
-                System.out.println("bienvenido");
-                PrincipalController.mostrar();
-                LoginController.ocultar();
-            }else{
-                System.out.println("no puede entrar");
-            }
-        }catch(Exception e){
-            System.out.println(e.getMessage());
-        }
-       
-        
-    }
 }
