343d4b8343e61b9140b3a9ac2017a4cee072ece0
Usuario <Usuario@DESKTOP-SI5VK9O>
2021-11-02 16:08:58 -0300

    controlador(jake)

--- PortalGestionDreamGifts/src/controlador/controlarLogin.java
+++ PortalGestionDreamGifts/src/controlador/controlarLogin.java
@@ -4,10 +4,33 @@
  */
 package controlador;
 
+import vista.ADM;
+import vista.login;
+
 /**
  *
  * @author Usuario
  */
 public class controlarLogin {
+    //Instancia login
+    
+    login v  = new login();
+    
+    String user = "todos";
+    String clave = "clave";
+    String usuario  = v.getTxtUsuario().getText();
+    String pass = new String(v.getTxtClave().getPassword()); 
+
+ 
+    
+    public void controlarLogin(){
+              
+                ADM a = new ADM();
+                 a.setVisible(true);
+                 v.dispose();   
+           
+    }
+    
+    
     
 }
