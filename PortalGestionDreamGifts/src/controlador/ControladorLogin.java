5b41b5667e1690345a4f57a970daf0da99dc9d3f
Luxarx <lucas.vargas@sansano.usm.cl>
2021-11-03 19:13:09 -0300

    Added ControladorLogin y Deleted el antiguo.

---
+++ PortalGestionDreamGifts/src/controlador/ControladorLogin.java
@@ -0,0 +1,37 @@
+package controlador;
+import vista.Login;
+import modelo.UsuarioManager;
+import vista.Maestro;
+/**
+ *
+ * @author luvar
+ */
+public class ControladorLogin {
+    
+    public void ocultar(Login vistaLogin){
+        vistaLogin.setVisible(false);
+    }
+    
+    public void menu(){
+        Maestro master = new Maestro();
+        master.setVisible(true);
+    }
+    
+    public void login(Login vistaLogin){
+        String usuario = vistaLogin.getTextoUsuario().toString();
+        String clave = vistaLogin.getTextoClave().toString();
+        
+        UsuarioManager check = new UsuarioManager();
+        try{
+            boolean auth = check.loginCheck(usuario, clave);
+            if(auth){
+                ocultar(vistaLogin);
+                menu();
+            }
+            
+        } catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        
+    }
+}
