67bc0f0382fa9d76e3c43a0bf5f44f9e8a1d37ce
Luxarx <65048397+Luxarx@users.noreply.github.com>
2021-11-05 03:30:23 +0000

    Starcrow (#22)
    * pruebas consultas y vista banco
    
    * insert
    
    * crud completo banco
    
    * Update BancoController.java
    
    Co-authored-by: xCrowstone <92612803+xCrowstone@users.noreply.github.com>

--- PortalGestionDreamGifts/src/modelo/Banco.java
+++ PortalGestionDreamGifts/src/modelo/Banco.java
@@ -4,6 +4,8 @@
  */
 package modelo;
 import bd.Consulta;
+
+import bd.Log;
 import java.sql.ResultSet;
 /**
  *
@@ -17,11 +19,10 @@ public class Banco {
 
     public Banco() {
     }
-
-    public Banco(int idBanco, String nombre, int enabled) {
-        this.idBanco = idBanco;
+    public Banco(String nombre) {
+       
         this.nombre = nombre;
-        this.enabled = enabled;
+        
     }
 
     @Override
@@ -53,17 +54,38 @@ public class Banco {
         this.enabled = enabled;
     }
     
-    public void agregarBanco(){
-        try{
-            Consulta con = new Consulta();
+
+    public boolean agregarBanco(String nombre){
+        //try{
+        Consulta con = new Consulta();
             
-            
-            String query = "INSERT INTO `banco`(`Nombre`) VALUES ('"+nombre+"'";
-            
-            ResultSet rs = con.insert(query);
-        }catch(Exception e){
-            System.out.println(e.getMessage());
-        }
+        String query = "INSERT INTO `banco`(`Nombre`) VALUES ('"+nombre+"')";
+        Log.seguir(query);
+        return con.ejecutar(query);
+            //ResultSet rs = con.insert(query);
+        //}catch(Exception e){
+        //    System.out.println(e.getMessage());
+        //}
+        //return false;
+    }
+    
+    public boolean actualizarBanco(String nombre)
+    {
+        Consulta con = new Consulta();
         
+        String query = String.format("UPDATE `banco` SET =`Nombre`='"+nombre+"' WHERE 1");
+        Log.seguir("estoy en update"+query);
+        return con.ejecutar(query);
+    }
+    
+    public boolean borrarBanco(String nombre)
+    {
+        Consulta con = new Consulta();
+        
+        String query = String.format("Delete from 'banco' "
+                           + "where nombre= '%s'"
+                              ,nombre);
+        Log.seguir("estoy en delete"+query);
+        return con.ejecutar(query);
     }
 }
