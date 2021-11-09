100489a7af7bb6e675e55c343a57f7db9f8d9f7a
xCrowstone <92612803+xCrowstone@users.noreply.github.com>
2021-11-08 21:06:25 -0300

    crud Statusdespacho

--- PortalGestionDreamGifts/src/deprecated/modelos/StatusDespacho.java
+++ PortalGestionDreamGifts/src/deprecated/modelos/StatusDespacho.java
@@ -4,6 +4,15 @@
  */
 package deprecated.modelos;
 
+import bd.Consulta;
+import bd.Log;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author PC
@@ -42,5 +51,40 @@ public class StatusDespacho {
         this.descripcionStatus = descripcionStatus;
     }
     
+    public boolean agregarStatusDespacho(String descripcionStatus)
+    {
+        Consulta con = new Consulta();
+        
+        String query = "INSERT INTO `statusDespacho`(`descripcionEstado`) VALUES ('"+descripcionStatus+"')";
+        Log.seguir("estoy en insert"+query);
+        return con.ejecutar(query);
+    }
     
+    public boolean borrarStatusDespacho(String descripcionStatus)
+    {
+        Consulta con = new Consulta();
+        
+        String query = String.format("Delete from 'statusDespacho' " + "where descripcionStatus= '%s'",descripcionStatus);
+        Log.seguir("estoy en delete"+query);
+        return con.ejecutar(query);
+    }
+    
+    public static List<StatusDespacho> listarRRSS() throws SQLException
+    {
+        List<StatusDespacho> lista =new ArrayList<>();
+          String query = String.format("select * from Status");
+        Log.seguir("termine de listar");
+        ResultSet curSta= bd.Consulta.getInstance().select(query);
+        try {
+            while(curSta.next())
+            {
+                StatusDespacho status =new StatusDespacho(curSta.getInt("idStatusDespacho"), curSta.getString("descripcionStatus"));
+                lista.add(status);    
+            }
+            return lista;
+        } catch (SQLException ex) {
+            Logger.getLogger(StatusDespacho.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
 }
