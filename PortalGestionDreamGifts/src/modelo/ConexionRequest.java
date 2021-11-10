10bd0a2245c8943b5830817047808192efc2a9b9
Luxarx <lucas.vargas@sansano.usm.cl>
2021-11-10 20:15:07 -0300

    Limpieza.

--- PortalGestionDreamGifts/src/modelo/ConexionRequest.java
+++ PortalGestionDreamGifts/src/modelo/ConexionRequest.java
@@ -8,21 +8,7 @@ import java.lang.*;
  *
  * @author luvar
  */
-//            -----[DEBUG] Codigo para preguntar input de cada credencial-----
-//            address = JOptionPane.showInputDialog(null,"IP Base de Datos","localhost");
-//            if(address.isEmpty()|| address == null) throw new Exception("Dirección Incorrecta");
-//            
-//            database = JOptionPane.showInputDialog("Nombre Base de Datos");
-//            if(database.isEmpty()|| database == null) throw new Exception("Base de Datos Incorrecta");
-//            
-//            user = JOptionPane.showInputDialog("Usuario");
-//            if(user.isEmpty()|| user == null) throw new Exception("Usuario Incorrecto");
-//            
-//            JPasswordField pwd = new JPasswordField(20);
-//            pass = "";
-//            int action = JOptionPane.showConfirmDialog(null, pwd,"Introducir Contraseña",JOptionPane.OK_CANCEL_OPTION);
-//            if(action < 0| action == 2) throw new Exception("Contraseña Incorrecta");
-//            else pass = new String(pwd.getPassword());
+
 public class ConexionRequest {
 
     public Connection conectar() {
