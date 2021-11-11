69a924737fd56fee02fab69f63ae47396268cbca
Luxarx <lucas.vargas@sansano.usm.cl>
2021-11-11 17:03:50 -0300

    Deprecated BancoManager

--- PortalGestionDreamGifts/src/deprecated/controladores/BancoController.java
+++ PortalGestionDreamGifts/src/deprecated/controladores/BancoController.java
@@ -2,8 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package controlador;
-import modelo.BancoManager;
+package deprecated.controladores;
+import deprecated.modelos.BancoManager;
 import bd.Consulta;
 import vista.Maestro;
 import java.sql.ResultSet;
