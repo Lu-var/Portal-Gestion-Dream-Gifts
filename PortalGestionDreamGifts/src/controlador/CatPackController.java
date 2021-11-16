ce0ef03f16fd17c72e6b35985207c3285aa11b7c
Luxarx <lucas.vargas@sansano.usm.cl>
2021-11-16 00:40:14 -0300

    Added Categorias Packs full.

---
+++ PortalGestionDreamGifts/src/controlador/CatPackController.java
@@ -0,0 +1,119 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+import bd.Log;
+import java.util.ArrayList;
+import java.util.Vector;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import modelo.CatPackManager;
+import vista.Maestro;
+
+/**
+ *
+ * @author luvar
+ */
+public class CatPackController {
+    
+    CatPackManager manager = new CatPackManager(); 
+    
+    public void agregarCategoria(Maestro vista){
+        String nombre = vista.getTxtCatPack().getText();
+        manager.agregarCategoriaSQL(nombre);
+    }
+
+    
+    public void showAll(Maestro master){
+        
+        JTable tabla = master.getTablaCatPack();
+        ArrayList<ArrayList<Object>> lista = manager.categoriaSelectAll();
+        DefaultTableModel model =  (DefaultTableModel)tabla.getModel();
+        
+        int i = 0;
+        
+        while(i<lista.size()){
+            model.addRow(lista.get(i).toArray());
+            i++;
+        }
+        
+        
+    }
+    
+    public void clearAll(Maestro master){
+        JTable tabla = master.getTablaCatPack();
+        DefaultTableModel model = (DefaultTableModel) tabla.getModel();
+        model.setRowCount(0);
+    }
+
+    public void edit(Maestro master){
+        
+        int id;
+        String nombre = new String();
+        String status;
+        boolean flagStatus;
+        
+        
+        JTable tabla = master.getTablaCatPack();
+        DefaultTableModel model = (DefaultTableModel) tabla.getModel();
+        int pos = tabla.getSelectedRow();
+        Object input;
+        try {
+            Vector fila = (Vector)model.getDataVector().elementAt(pos);
+            
+            if((boolean)fila.get(2)){
+                status = "Activado";
+            }
+            
+            else{
+                status = "Desactivado";
+            }
+            
+//            input = JOptionPane.showInputDialog(null, "Nuevo ID", fila.get(0));
+//            id = Integer.valueOf((String)input);
+//            for (int j = 0; j < model.getRowCount(); j++) {
+//                int get = (int)model.getValueAt(j, 0);
+//                if(get==id && id != (int)fila.get(0)){
+//                    Log.seguir("ID Ocupado.");
+//                    JOptionPane.showMessageDialog(null, "ID Ocupado.");
+//                    return;
+//                }
+//            }
+            try {
+                input = JOptionPane.showInputDialog(null, "Nueva Categoria", fila.get(1));
+                nombre = (String)input;
+                if(nombre.isEmpty()){
+                    Log.seguir("Campo vacío.");
+                    JOptionPane.showMessageDialog(null, "Campo Vacio",null,JOptionPane.INFORMATION_MESSAGE);
+                    return;
+            }
+            } catch (java.lang.NullPointerException ex) {
+                JOptionPane.showMessageDialog(null, "Edición Cancelada", null, JOptionPane.INFORMATION_MESSAGE);
+                Log.seguir(ex.getMessage());
+                return;
+            }
+
+
+            input = JOptionPane.showConfirmDialog(null,"¿Activar esta Categoría? ","Estado Actual: " + status, JOptionPane.YES_NO_OPTION);
+            if((int)input == JOptionPane.CLOSED_OPTION){
+                JOptionPane.showMessageDialog(null, "Edición Cancelada", null, JOptionPane.INFORMATION_MESSAGE);
+                return;
+            }  
+            flagStatus = (int)input == 0;
+            
+//            tabla.setValueAt(id, pos, 0);
+            tabla.setValueAt(nombre, pos, 1);
+            tabla.setValueAt(flagStatus, pos, 2);
+            
+            
+            manager.update((int)fila.get(0), nombre, (boolean)fila.get(2));
+            
+        } catch (Exception ex) {
+            Log.seguir(ex.getMessage());
+            JOptionPane.showMessageDialog(null, "No hay fila seleccionada");
+        }
+    }
+    
+}
---
+++ PortalGestionDreamGifts/src/modelo/CatPackManager.java
@@ -0,0 +1,152 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+import bd.Log;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author PC
+ */
+public class CatPackManager {
+
+    ConexionRequest intentoConexion = new ConexionRequest();
+    Connection conexion = intentoConexion.conectar();
+
+    PreparedStatement comando = null;
+    boolean resultadoBool;
+    ResultSet resultadoSet = null;
+    
+
+    public ArrayList<ArrayList<Object>> categoriaSelectAll(){
+        ArrayList<ArrayList<Object>> matriz = new ArrayList<ArrayList<Object>>();
+        try{
+            comando = conexion.prepareStatement("SELECT * FROM CategoriaPack");
+            resultadoSet = comando.executeQuery();
+
+            int id;
+            int status;
+            String desc;
+            while(resultadoSet.next()){
+                ArrayList<Object> fila = new ArrayList<Object>();
+
+                id = resultadoSet.getInt("idCategoriaPack");
+                status = resultadoSet.getInt("Enabled");
+                desc = resultadoSet.getString("Descripcion");
+
+                switch(status){
+                    case 0:
+                        resultadoBool = false;
+                    case 1:
+                        resultadoBool = true;
+
+                    default:
+                        break;
+                }
+
+                fila.add(id);
+                fila.add(desc);
+                fila.add(resultadoBool);
+                
+                matriz.add(fila);
+            }
+            
+        } catch(Exception ex){
+            JOptionPane.showMessageDialog(null,"Error: "+ex);
+        }
+        
+        return matriz;
+    }
+    
+    public String categoriaIDSelect(int id){
+        String nombre = new String();
+        try {
+            PreparedStatement comando = conexion.prepareStatement("SELECT Descripcion FROM Pack WHERE idCategoriaPack = ?");
+            
+            comando.setInt(1, id);
+            
+            ResultSet resultado = comando.executeQuery();
+            
+            resultado.next();
+            nombre = resultado.getString(1);
+            
+        } catch (Exception ex) {
+            Log.seguir(ex.getMessage());
+        }
+        
+        return nombre;
+    }
+    
+    public ArrayList<ArrayList<Object>> categoriasEnabledSelectAll(){
+        ArrayList<ArrayList<Object>> matriz = new ArrayList<ArrayList<Object>>();
+        try{
+            comando = conexion.prepareStatement("SELECT * FROM CategoriaPack WHERE Enabled = 1");
+            resultadoSet = comando.executeQuery();
+
+            int id;
+            String desc;
+            while(resultadoSet.next()){
+                ArrayList<Object> fila = new ArrayList<Object>();
+
+                id = resultadoSet.getInt("idCategoriaPack");
+                desc = resultadoSet.getString("Descripcion");
+
+
+                fila.add(id);
+                fila.add(desc);
+                
+                matriz.add(fila);
+            }
+            
+        } catch(Exception ex){
+            JOptionPane.showMessageDialog(null,"Error: "+ex);
+        }
+        
+        return matriz;
+    }    
+
+    public void agregarCategoriaSQL(String descripcion){
+        PreparedStatement comando = null;
+
+        try{
+            comando = conexion.prepareStatement("INSERT INTO CategoriaPack (Descripcion) VALUES (?)");
+            comando.setString(1, descripcion);
+            comando.execute();
+            
+
+        } catch(SQLException ex){
+            JOptionPane.showMessageDialog(null,ex);
+        }  
+    }
+    
+    
+    public void update(int idTarget, String nombre, boolean flag){
+        
+        int status;
+        if(flag){status = 1;} else{status = 0;}
+        
+        try{
+            comando = conexion.prepareStatement("UPDATE CategoriaPack SET Descripcion = (?), Enabled = (?) WHERE idCategoriaPack = (?)");
+            
+            comando.setString(1, nombre);
+            comando.setInt(2,status);
+            comando.setInt(3, idTarget);
+            
+            comando.execute();
+            
+        } catch(Exception ex){
+            Log.seguir(ex.getMessage());
+        }
+        
+        
+    }
+}
